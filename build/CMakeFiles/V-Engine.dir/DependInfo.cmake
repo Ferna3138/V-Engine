089ee12c78a03b76d2147da7f32bd864a727c208
Fernando Edelstein <fernandoedelstein99@gmail.com>
2025-03-18 16:12:36 +0100

    Mac ignore

---
+++ build/CMakeFiles/V-Engine.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/fernandoedelstein/Documents/GitHub/V-Engine/src/backend/glfw_backend.cpp" "CMakeFiles/V-Engine.dir/src/backend/glfw_backend.cpp.o" "gcc" "CMakeFiles/V-Engine.dir/src/backend/glfw_backend.cpp.o.d"
+  "/Users/fernandoedelstein/Documents/GitHub/V-Engine/src/controller/app.cpp" "CMakeFiles/V-Engine.dir/src/controller/app.cpp.o" "gcc" "CMakeFiles/V-Engine.dir/src/controller/app.cpp.o.d"
+  "/Users/fernandoedelstein/Documents/GitHub/V-Engine/src/logging/logger.cpp" "CMakeFiles/V-Engine.dir/src/logging/logger.cpp.o" "gcc" "CMakeFiles/V-Engine.dir/src/logging/logger.cpp.o.d"
+  "/Users/fernandoedelstein/Documents/GitHub/V-Engine/src/main.cpp" "CMakeFiles/V-Engine.dir/src/main.cpp.o" "gcc" "CMakeFiles/V-Engine.dir/src/main.cpp.o.d"
+  "/Users/fernandoedelstein/Documents/GitHub/V-Engine/src/renderer/device.cpp" "CMakeFiles/V-Engine.dir/src/renderer/device.cpp.o" "gcc" "CMakeFiles/V-Engine.dir/src/renderer/device.cpp.o.d"
+  "/Users/fernandoedelstein/Documents/GitHub/V-Engine/src/renderer/instance.cpp" "CMakeFiles/V-Engine.dir/src/renderer/instance.cpp.o" "gcc" "CMakeFiles/V-Engine.dir/src/renderer/instance.cpp.o.d"
+  "/Users/fernandoedelstein/Documents/GitHub/V-Engine/src/renderer/renderer.cpp" "CMakeFiles/V-Engine.dir/src/renderer/renderer.cpp.o" "gcc" "CMakeFiles/V-Engine.dir/src/renderer/renderer.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
